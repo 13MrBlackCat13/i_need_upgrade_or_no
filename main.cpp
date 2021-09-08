e17594b345456198c2b9d1022e470bfd97e3c7c5
Михаил <69348201+13MrBlackCat13@users.noreply.github.com>
2021-09-08 17:54:26 +0200

    make it better

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int ram;
+   cin >> ram;
+    // место для вашего кода
+    if(ram < 16){
+        cout << "PC upgrade needed";
+    }
+
+    return 0;
+}
